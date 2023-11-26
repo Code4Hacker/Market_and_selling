742ccacdf393b6d963864a5304ab90a9d5324745
Code4Hacker <black2022mail@gmail.com>
2023-11-26 18:48:53 +0300

    Register

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,20 +6,22 @@ import bgone from './../../assets/images/bgone.jpeg';
 const Login = () => {
   return (
     <div className="login">
-      <div className='row' style={{
+      <div className="" style={{
         backgroundImage:`url(${bgone})`,
         backgroundSize:'cover',
       }}>
-        <div className="col-md-6">
+      <div className='row blur'>
+        <div className="col-md-5">
         </div>
-        <div className="col-md-6 grid">
+        <div className="col-md-7 grid">
           <div className="body_reg">
-            <div className="title">
+            <div className="title mt-2">
               <h2>Sign In</h2>
             </div>
             <div className="inputs">
               <input type="text" placeholder='Username'/>
               <input type="text" placeholder='Password'/>
+              <button className='primary w-50'>Login</button>
               <div className="remember_me mt-2">
                 <input type="checkbox" name="" id="" /> Remember Me
               </div>
@@ -27,9 +29,17 @@ const Login = () => {
                 <p className='mt-4'>I don’t have an Account. <Link to={'/creator'} className='link'>Create One</Link>.</p>
                 <p className='mt-1'><Link to={'/creator'} className='link' style={{fontSize:'15px'}}>Forget Password ?</Link></p>
               </div>
+              <div className="icons mt-3">
+                <a href='#'><i className="bi bi-github"></i></a>
+                <a href='#'><i className="bi bi-instagram"></i></a>
+                <a href='#'><i className="bi bi-linkedin"></i></a>
+                <a href='#'><i className="bi bi-youtube"></i></a>
+                <a href='#'><i className="bi bi-pinterest"></i></a>
+              </div>
             </div>
           </div>
         </div>
+      </div>
       </div>
     </div>
   )
