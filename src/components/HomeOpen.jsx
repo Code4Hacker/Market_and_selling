40aa3bb4f04d920bbe298a8fba4347e79e986930
Code4Hacker <black2022mail@gmail.com>
2023-12-02 18:20:09 +0300

    CHANGES

--- src/components/HomeOpen.jsx
+++ src/components/HomeOpen.jsx
@@ -11,21 +11,24 @@ const HomeOpen = () => {
           <div className="container">
             <div className="banner">
               <div className="banner_header">
-                <h1 className='animation__animate animate__animated animate__fadeInUp'> The Gemini <div className="box_notify"><span className='txt-grad' style={{ color: 'black' }}> Market </span></div> Online. RECEIVE  BETTER</h1>
+                <h1 className='animation__animate animate__animated animate__fadeInUp box_notify'> The Gemini <span className='txt-grad' style={{ color: 'black' }}> Market </span> Online. RECEIVE  BETTER</h1>
                 <div className="animation__animate animate__animated animate__fadeInUp  animate__delay-5s">
                   <span className='mini-txt'> Sell or Buy Products online in 100% Guaranteed Promo. Our Role is to  bring trust to the Clients</span>
+                </div>
+                <div className="button mt-5 pt-5 pb-5">
+                  <Link to={'/sign_'} className='bi bi-stack primary btn-success'> Start  Exploring</Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="bubble-animated">
-      </div>
+      {/* <div className="bubble-animated">
+      </div> */}
       <div className="container">
         <div className="banner">
           <div className="banner_header">
-            <h1 className='animation__animate animate__animated animate__fadeInUp'> The Gemini <div className="box_notify"><span className='txt-grad box-txt '> MARKET </span></div> Online. RECEIVE  BETTER</h1>
+            <h1 className='animation__animate animate__animated animate__fadeInUp box_notify'> The Gemini <span className='txt-grad box-txt '> MARKET </span> Online. RECEIVE  BETTER</h1>
             <span className='mini-txt'> Sell or Buy Products online in 100% Guaranteed Promo. Our Role is to  bring trust to the Clients</span>
           </div>
           <div className="button mt-5 pt-5 pb-5">
